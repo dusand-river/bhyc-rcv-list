2db124de652f35e8f40202aa8b2fce179d212171
Dusan <dusandavidovic@gmail.com>
2023-05-05 19:45:54 -0400

    moved vars to .env

--- src/config/sheets.ts
+++ src/config/sheets.ts
@@ -6,14 +6,5 @@ const SHEETS = {
     },
   },
   apiKey: import.meta.env.VITE_API_KEY, // sheetAPI project
-  // oAuth: {
-  //   client_id: "731023753425-l6pu6gl60tnsvl1688oo9hbdk0e4n304.apps.googleusercontent.com",
-  //   project_id: "sheetapi-382114",
-  //   auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  //   token_uri: "https://oauth2.googleapis.com/token",
-  //   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  //   client_secret: "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",
-  //   redirect_uris: ["http://localhost"],
-  // },
 };
 export default SHEETS;
